8b696387f87f98ebe2f4eb31e850e124d9db825d
Stefan Kolaric <stefan.kolaric4@gmail.com>
2023-09-20 22:31:30 +0200

    Made some changes to numbering the file table rows

--- TeamManagementApp/wwwroot/js/site.js
+++ TeamManagementApp/wwwroot/js/site.js
@@ -5,3 +5,12 @@
         "paging": true
     });
 });
+
+$(document).ready(function () {
+    $('#filesTable').DataTable({
+        "scrollY": "450px",
+        "scrollCollapse": true,
+        "paging": true,
+        "order": [[4, 'desc']]
+    });
+});
